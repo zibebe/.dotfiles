6f02d5baf6553b7e9436f4a4c1e02606482b319c
Tobias Tschinkowitz <tobias@truehumandesign.net>
2024-10-12 17:56:45 +0200

    (fish): modify git prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -94,3 +94,7 @@ set -gx FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS"\
 " --color=bg+:#3b4252,bg:#2e3440,spinner:#88c0d0,hl:#81a1c1"\
 " --color=fg:#d8dee9,header:#81a1c1,info:#ebcb8b,pointer:#88c0d0"\
 " --color=marker:#88c0d0,fg+:#eceff4,prompt:#ebcb8b,hl+:#81a1c1"
+
+# Fish git prompt
+set __fish_git_prompt_showuntrackedfiles yes
+set __fish_git_prompt_showdirtystate yes
