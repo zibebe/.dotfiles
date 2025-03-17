a1069c13cb8b876215ae975fdd8b26bf6c0ee8a9
Tobias Tschinkowitz <me@zibebe.net>
2025-03-17 12:55:59 +0100

    fu2

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = "Kanagawa (Gogh)"
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.font = wezterm.font("Fira Code Retina")
+config.font_size = 16.0
+config.line_height = 1.1
+config.front_end = "WebGpu"
+config.native_macos_fullscreen_mode = true
+config.window_decorations = "RESIZE"
+config.audible_bell = "Disabled"
+
+return config
