628ac58321d0e3c14933f81540368a29f50f8162
Tobias Tschinkowitz <me@zibebe.net>
2025-03-20 21:58:45 +0100

    update brewfile

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,12 +5,5 @@ end
 set -g fish_greeting
 
 if command -q eza
-    abbr -a l eza
     abbr -a ls eza
-    abbr -a ll 'eza -l'
-    abbr -a lll 'eza -la'
-else
-    abbr -a l ls
-    abbr -a ll 'ls -l'
-    abbr -a lll 'ls -la'
 end
