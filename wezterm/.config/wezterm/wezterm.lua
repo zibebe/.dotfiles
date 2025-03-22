304893795ec024c0e1eb604fc6142badd2b1221c
Tobias Tschinkowitz <me@zibebe.net>
2025-03-22 14:09:00 +0100

    (wezterm): config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,7 +8,6 @@ config.font = wezterm.font("Fira Code Retina")
 config.font_size = 16.0
 config.line_height = 1.1
 config.front_end = "WebGpu"
-config.native_macos_fullscreen_mode = true
 config.window_decorations = "RESIZE"
 config.audible_bell = "Disabled"
 
