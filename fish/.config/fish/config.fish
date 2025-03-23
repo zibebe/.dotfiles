28ce450dc969489f3fa6e582c96e5f4d8dcd3365
Tobias Tschinkowitz <me@zibebe.net>
2025-03-23 14:57:23 +0100

    (eza): remove eza

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,11 +2,6 @@ if status is-interactive
     # Disable greeting
     set -g fish_greeting
 
-    # Modern ls
-    if command -q eza
-        abbr -a ls eza
-    end
-
     # Load fnm
     fnm env --use-on-cd --shell fish | source
 
