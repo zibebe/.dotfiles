a22f3f2b61159fc0ce0b8379f8aeea6929091077
Tobias Tschinkowitz <me@zibebe.net>
2025-03-23 09:07:16 +0100

    modus themes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,6 +7,13 @@ if status is-interactive
         abbr -a ls eza
     end
 
+    # Set fish highlighting according to system settings
+    if test "$(defaults read -g AppleInterfaceStyle 2>/dev/null)" = Dark
+        source ~/.config/fish/themes/modus_vivendi.fish
+    else
+        source ~/.config/fish/themes/modus_operandi.fish
+    end
+
     # Load fnm
     fnm env --use-on-cd --shell fish | source
 
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,51 +1,22 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- Kanagwa Color Scheme took from
--- https://github.com/rebelot/kanagawa.nvim/blob/master/extras/wezterm/kanagawa.lua
-config.force_reverse_video_cursor = true
-config.colors = {
-  foreground = "#dcd7ba",
-  background = "#1f1f28",
+local function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return "Dark"
+end
 
-  cursor_bg = "#c8c093",
-  cursor_fg = "#c8c093",
-  cursor_border = "#c8c093",
+local function scheme_for_appearance(appearance)
+  if appearance:find "Dark" then
+    return "Modus Vivendi"
+  else
+    return "Modus Operandi"
+  end
+end
 
-  selection_fg = "#c8c093",
-  selection_bg = "#2d4f67",
-
-  scrollbar_thumb = "#16161d",
-  split = "#16161d",
-
-  ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-  brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-  indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-
-  tab_bar = {
-    background = "#1f1f28",
-    active_tab = {
-      fg_color = "#1f1f28",
-      bg_color = "#c0a36e",
-    },
-    inactive_tab = {
-      fg_color = "#1f1f28",
-      bg_color = "#727169",
-    },
-    inactive_tab_hover = {
-      fg_color = "#c0a36e",
-      bg_color = "#727169",
-    },
-    new_tab = {
-      fg_color = "#c0a36e",
-      bg_color = "#1f1f28",
-    },
-    new_tab_hover = {
-      fg_color = "#c0a36e",
-      bg_color = "#1f1f28",
-    }
-  }
-}
+config.color_scheme = scheme_for_appearance(get_appearance())
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.font = wezterm.font("Fira Code Retina")
