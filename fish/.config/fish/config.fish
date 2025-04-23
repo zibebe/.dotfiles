6db49a0ad95b0f36a6ab4b4af7e54c16e04bf889
Tobias Tschinkowitz <me@zibebe.net>
2025-04-23 19:29:52 +0200

    (tv): television instead of fzf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -44,23 +44,22 @@ if status is-interactive
         abbr -a cat bat
     end
 
-    # Set up fzf
-    set -gx FZF_DEFAULT_COMMAND "fd --type f --hidden --follow --exclude \".git\""
-    set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
-    set -gx FZF_ALT_C_COMMAND "fd --type d --hidden --follow --exclude \".git\""
-    set -gx FZF_DEFAULT_OPTS '--color=fg:#e5e9f0,bg:#2E3440,hl:#81a1c1
-    --color=fg+:#e5e9f0,bg+:#2E3440,hl+:#81a1c1
-    --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac
-    --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b'
-    fzf --fish | source
+    # Set up television
+    if type -q tv
+        tv init fish | source
+    end
 
     # fnm (node version manager)
-    fnm env --use-on-cd --shell fish | source
+    if type -q fnm
+        fnm env --use-on-cd --shell fish | source
+    end
 
     # k9s config path
     set -gx K9S_CONFIG_DIR $HOME/.config/k9s
 
     # Load starship
-    starship init fish | source
-    enable_transience
+    if type -q starship
+        starship init fish | source
+        enable_transience
+    end
 end
