9d59c58eeb70a744aa44d0bbd51807f1f7a9ebf3
Tobias Tschinkowitz <me@zibebe.net>
2025-04-25 08:23:32 +0200

    (wezterm): impl font toggler

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -38,13 +38,41 @@ config.color_schemes = {
 config.color_scheme = "nord"
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
--- config.font = wezterm.font("Comic Code Ligatures")
--- config.line_height = 1.2
 config.font = wezterm.font("Codelia Ligatures")
 config.line_height = 1.1
 config.font_size = 18.0
 config.front_end = "WebGpu"
 config.window_decorations = "RESIZE"
 config.audible_bell = "Disabled"
+
+wezterm.on('toggle-font', function(window, _)
+  local overrides = window:get_config_overrides() or {}
+
+  local current_font_family
+
+  if overrides.font then
+    current_font_family = overrides.font.font[1].family
+  else
+    current_font_family = config.font.font[1].family
+  end
+
+  if current_font_family == "Codelia Ligatures" then
+    overrides.font = wezterm.font("Comic Code Ligatures")
+    overrides.line_height = 1.2
+  else
+    overrides.font = wezterm.font("Codelia Ligatures")
+    overrides.line_height = 1.1
+  end
+
+  window:set_config_overrides(overrides)
+end)
+
+config.keys = {
+  {
+    key = 'f',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.EmitEvent 'toggle-font',
+  },
+}
 
 return config
