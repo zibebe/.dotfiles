d51b371414c76d4cc6946ad3d29aabd86c94b4ee
Tobias Tschinkowitz <me@zibebe.net>
2025-04-25 08:51:56 +0200

    (wezterm): improve font toggler

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -38,25 +38,21 @@ config.color_schemes = {
 config.color_scheme = "nord"
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
-config.font = wezterm.font("Codelia Ligatures")
-config.line_height = 1.1
-config.font_size = 18.0
 config.front_end = "WebGpu"
 config.window_decorations = "RESIZE"
 config.audible_bell = "Disabled"
+config.font = wezterm.font("Codelia Ligatures")
+config.line_height = 1.1
+config.font_size = 18.0
 
 wezterm.on('toggle-font', function(window, _)
   local overrides = window:get_config_overrides() or {}
 
-  local current_font_family
+  local current_config = overrides.font or config.font
+  local current_family = current_config and current_config.font and current_config.font[1] and
+      current_config.font[1].family or ""
 
-  if overrides.font then
-    current_font_family = overrides.font.font[1].family
-  else
-    current_font_family = config.font.font[1].family
-  end
-
-  if current_font_family == "Codelia Ligatures" then
+  if current_family == "Codelia Ligatures" then
     overrides.font = wezterm.font("Comic Code Ligatures")
     overrides.line_height = 1.2
   else
