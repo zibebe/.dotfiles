6d771e7c47818e9b1f87dd6510e6a73325eca32f
Tobias Tschinkowitz <me@zibebe.net>
2025-10-21 17:20:31 +0200

    sf-mono font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,12 +1,11 @@
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
 local mux = wezterm.mux
+local config = wezterm.config_builder()
+local appearance = wezterm.gui.get_appearance()
 
 local function scheme_for_appearance()
-	local is_dark = wezterm.gui.get_appearance() == "Dark"
-
-	if is_dark then
- 		os.execute([[sed -i '' 's/^theme = ".*"$/theme = "modus_vivendi"/' /Users/zibebe/.config/helix/config.toml]])
+	if appearance:find "Dark" then
+		os.execute([[sed -i '' 's/^theme = ".*"$/theme = "modus_vivendi"/' /Users/zibebe/.config/helix/config.toml]])
 		os.execute("pkill -USR1 hx")
 		os.execute([[/opt/homebrew/bin/fish -c 'echo y | fish_config theme save "Modus Vivendi"']])
 		os.execute("cp /Users/zibebe/.config/eza/modus_vivendi.yml /Users/zibebe/.config/eza/theme.yml")
@@ -24,13 +23,15 @@ local function scheme_for_appearance()
 	end
 end
 
+config.front_end = "WebGpu"
+config.max_fps = 120
 config.color_scheme = scheme_for_appearance()
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.window_decorations = "RESIZE"
 config.audible_bell = "Disabled"
-config.font = wezterm.font 'SF Mono'
-config.line_height = 1.2
+config.font = wezterm.font "SF Mono"
+config.line_height = 1.15
 config.font_size = 18.0
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = false
@@ -39,7 +40,5 @@ wezterm.on('gui-startup', function(cmd)
 	local _, _, window = mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
 end)
-
-
 
 return config
