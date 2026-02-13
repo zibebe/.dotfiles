39733fc77743890fe3a3328e8fee02ee304103bf
Tobias Eckert <me@zibebe.net>
2026-02-13 13:25:50 +0100

    update fish_prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,15 +1,15 @@
 function fish_prompt
-    set_color blue
+    set_color bryellow
     echo -n (hostname -s)
     if [ $PWD != $HOME ]
         set_color brblack
         echo -n ':'
-        set_color yellow
+        set_color brblue
         echo -n (basename $PWD)
     end
-    set_color green
+    set_color brmagenta
     printf '%s ' (__fish_git_prompt)
-    set_color red
+    set_color brred
     echo -n '| '
     set_color normal
 end
