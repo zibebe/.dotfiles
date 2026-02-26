395edaac562e26879451489b5c6fa232cf970ebf
Tobias Eckert <me@zibebe.net>
2026-02-26 18:26:01 +0100

    gitconfig path

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,16 +1,14 @@
 function fish_prompt
-    set_color brblack
-    echo -n "["(date "+%H:%M")"] "
-    set_color blue
+    set_color brblue
     echo -n (command -q hostname; and hostname; or hostnamectl hostname)
     if [ $PWD != $HOME ]
         set_color brblack
         echo -n ':'
-        set_color yellow
+        set_color bryellow
         echo -n (basename $PWD)
     end
-    set_color green
+    set_color brgreen
     printf '%s\n' (__fish_git_prompt)
-    set_color normal
+    set_color brgreen
     echo 'λ '
 end
