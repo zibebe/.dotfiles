3d026b870f63523883c573c1be62803baea6c723
Tobias Eckert <me@zibebe.net>
2026-02-27 23:01:57 +0100

    fish git prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,15 +1,15 @@
 function fish_prompt
-    set_color brblue
+    set_color blue
     echo -n (command -q hostname; and hostname; or hostnamectl hostname)
     if [ $PWD != $HOME ]
         set_color brblack
         echo -n ':'
-        set_color bryellow
+        set_color yellow
         echo -n (basename $PWD)
     end
-    set_color brgreen
+    set_color green
     printf '%s\n' (__fish_git_prompt)
-    set_color brgreen
+    set_color red
     echo -n 'λ '
     set_color normal
 end
