350e7c2bbb01faa17c53420b32a3c7a6b799bfb1
Tobias Eckert <me@zibebe.net>
2026-02-27 10:55:09 +0100

    fish git prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -51,4 +51,10 @@ if status is-interactive
         abbr -a ll 'ls -l'
         abbr -a lll 'ls -la'
     end
+
+    # Fish git prompt
+    set __fish_git_prompt_showuntrackedfiles yes
+    set __fish_git_prompt_showdirtystate yes
+    set __fish_git_prompt_showupstream informative
+
 end
