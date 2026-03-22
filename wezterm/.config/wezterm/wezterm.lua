157f0377136ec61ffe2191a2104b1411f84aad27
Tobias Eckert <me@zibebe.net>
2026-03-22 16:29:50 +0100

    modus

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,43 +1,9 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-local scheme = wezterm.get_builtin_color_schemes()["Gruvbox dark, hard (base16)"]
-
-scheme.tab_bar = {
-  background = scheme.indexed[19],
-
-  active_tab = {
-    bg_color = "#d79921",
-    fg_color = scheme.background,
-  },
-
-  inactive_tab = {
-    bg_color = scheme.indexed[18],
-    fg_color = scheme.indexed[21],
-  },
-
-  inactive_tab_hover = {
-    bg_color = scheme.indexed[19],
-    fg_color = "#d79921",
-  },
-
-  new_tab = {
-    bg_color = scheme.indexed[18],
-    fg_color = scheme.indexed[21],
-  },
-
-  new_tab_hover = {
-    bg_color = scheme.indexed[18],
-    fg_color = scheme.ansi[4],
-  },
-}
-
-config.color_schemes = {
-  ["Gruvbox dark, hard (base16)"] = scheme
-}
 
 config.front_end = "WebGpu"
 config.max_fps = 120
-config.color_scheme = "Gruvbox dark, hard (base16)"
+config.color_scheme = "Modus Vivendi"
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.window_decorations = "RESIZE"
