6c135b7e5c60328abfc7b7ac56ff65ad6aec6aa7
Tobias Eckert <me@zibebe.net>
2026-03-23 20:00:05 +0100

    cleanup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 # Various globals
-set -gx EDITOR hx
+setenv EDITOR hx
 
 # Local binaries
 fish_add_path $HOME/.local/bin
@@ -8,7 +8,7 @@ fish_add_path $HOME/.local/bin
 /opt/homebrew/bin/brew shellenv | source
 
 # Go
-set -gx GOPATH $HOME/Developer/go
+setenv GOPATH $HOME/Developer/go
 fish_add_path $GOPATH/bin
 
 # PostgreSQL (libpq)
@@ -18,27 +18,29 @@ fish_add_path $HOMEBREW_PREFIX/opt/libpq/bin
 fish_add_path $HOME/.cargo/bin
 
 # EZA
-set -gx EZA_CONFIG_DIR $HOME/.config/eza
+setenv EZA_CONFIG_DIR $HOME/.config/eza
 
 if status is-interactive
     # Disable greeting
     set -g fish_greeting
 
+    # Fish git prompt
+    set __fish_git_prompt_showuntrackedfiles yes
+    set __fish_git_prompt_showdirtystate yes
+    set __fish_git_prompt_showstashstate yes
+    set __fish_git_prompt_showupstream informative
+
     # Various useful abbreviations
     abbr -a c cargo
-    abbr -a g git
-    abbr -a lg lazygit
+    abbr -a ct 'cargo test'
     abbr -a e hx
+    abbr -a g git
     abbr -a gc 'git checkout'
     abbr -a ga 'git add -p'
+    abbr -a gah 'git stash; and git pull --rebase; and git stash pop'
     abbr -a pr 'gh pr create -t "$(git show -s --format=%s HEAD)" -b "$(git show -s --format=%B HEAD | tail -n+3)"'
 
-    # fnm (node version manager)
-    if type -q fnm
-        fnm env --use-on-cd --shell fish | source
-    end
-
-    # modern ls
+    # eza (modern ls)
     if type -q eza
         abbr -a l eza
         abbr -a ls eza
@@ -50,9 +52,23 @@ if status is-interactive
         abbr -a lll 'ls -la'
     end
 
-    # Fish git prompt
-    set __fish_git_prompt_showuntrackedfiles yes
-    set __fish_git_prompt_showdirtystate yes
-    set __fish_git_prompt_showupstream informative
+    # Allow Ctrl-z to toggle between suspend and resume
+    bind \cz 'fg 2>/dev/null; commandline -f repaint'
 
+    # fzf (fuzzy file finder)
+    if type -q fzf
+        fzf --fish | source
+        setenv FZF_DEFAULT_COMMAND 'fd --type file --follow'
+        setenv FZF_CTRL_T_COMMAND 'fd --type file --follow'
+    end
+
+    # fnm (node version manager)
+    if type -q fnm
+        fnm env --use-on-cd --shell fish | source
+    end
+
+    # zoxide (smater cd)
+    if type -q zoxide
+        zoxide init fish | source
+    end
 end
