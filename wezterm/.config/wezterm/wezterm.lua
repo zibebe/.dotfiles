796f8d44ee9acb1e94a7988c8c4c8622b3f5f4d9
Tobias Eckert <me@zibebe.net>
2026-04-01 18:04:25 +0200

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -12,12 +12,4 @@ config.font = wezterm.font "SF Mono"
 config.line_height = 1.3
 config.font_size = 18
 
-config.keys = {
-  {
-    key = "Enter",
-    mods = "SHIFT",
-    action = wezterm.action.SendString("\x1b[13;2u"),
-  }
-}
-
 return config
